c0b6dd7accde2e1d51d1b2107b5aa6134bf3544f
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-02-18 22:58:55 -0300

    Use cache components

--- shared/metadataStore.ts
+++ shared/metadataStore.ts
@@ -1,5 +1,6 @@
 import { Redis } from '@upstash/redis'
 import { AssetMetadata, AssetMetadataUpdate } from './assets'
+import { cacheTag } from 'next/cache'
 
 type AssetMetadataValue = Omit<AssetMetadata, 'id'>
 
@@ -22,6 +23,8 @@ export async function getAllAssetMetadata({ project, force }: {
     project: string,
     force?: boolean,
 }) {
+    'use cache'
+    cacheTag(`${project}:assets:index`)
     if (force || !allAssets[project]) {
         allAssets[project] = await loadAllAssetMetadata({ project })
         setTimeout(invalidateCache, 1000 * 60 * 1) // Invalidate cache after 1 minute
@@ -30,6 +33,8 @@ export async function getAllAssetMetadata({ project, force }: {
 }
 
 export async function getAssetMetadata({ id, project }: { id: string, project: string }) {
+    'use cache'
+    cacheTag(`${project}:asset:${id}`)
     if (null !== allAssets) {
         const asset = allAssets[project]?.find((asset) => asset.id === id)
         if (asset) {
@@ -40,6 +45,8 @@ export async function getAssetMetadata({ id, project }: { id: string, project: s
 }
 
 export async function getAssetIds({ project }: { project: string }) {
+    'use cache'
+    cacheTag(`${project}:assets:index`)
     return redis.hkeys(assetsKey(project))
 }
 
