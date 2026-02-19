deef265e694238fac9ac8eb49f54c94bf3f1fdd7
Anton Shakirzianov <ashakirzianov@icloud.com>
2026-02-18 22:44:03 -0300

    Support revalidation hooks in ProjectConfig

--- shared/projects.ts
+++ shared/projects.ts
@@ -2,6 +2,8 @@ export type ProjectConfig = {
     users: string[],
     secret: string,
     makeExternalLink?(assetId: string): string,
+    revalidateAsset?(assetId: string): Promise<boolean>,
+    revalidateAssetIndex?(): Promise<boolean>,
 }
 
 export function getProjectConfig(project: string): ProjectConfig | undefined {
@@ -14,11 +16,36 @@ const projects: Record<string, ProjectConfig> = {
 
 function makeAlikroConfig(): ProjectConfig {
     const domain = process.env.ALIKRO_DOMAIN ?? 'alikro.art'
+    async function revalidateTag(tag: string) {
+        try {
+            // Call https://{domain}/api/revalidate/{tag} to revalidate the tag
+            const res = await fetch(`https://${domain}/api/revalidate/${tag}`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${process.env.ALIKRO_SECRET_KEY ?? 'alikro'}`,
+                }
+            })
+            if (!res.ok) {
+                console.error(`Failed to revalidate tag "${tag}". Status: ${res.status}, Response: ${await res.text()}`)
+                return false
+            }
+            return true
+        } catch (error) {
+            console.error('Error revalidating tag:', error)
+            return false
+        }
+    }
     return {
         users: ['alikro', 'ashakirzianov'],
         secret: process.env.ALIKRO_SECRET_KEY ?? 'alikro',
         makeExternalLink(assetId: string) {
             return `https://${domain}/all/${assetId}`
-        }
+        },
+        async revalidateAsset(assetId: string) {
+            return revalidateTag(`asset:${assetId}`)
+        },
+        async revalidateAssetIndex() {
+            return revalidateTag(`assets:index`)
+        },
     }
 }
